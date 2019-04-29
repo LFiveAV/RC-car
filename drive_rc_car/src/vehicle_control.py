23dd12976870b7e60df968d76c61d06377494a6e
Karl <27566241+krlhrnbrnt@users.noreply.github.com>
2019-04-29 11:43:55 +0200

    Add files via upload

--- drive_rc_car/src/vehicle_control.py
+++ drive_rc_car/src/vehicle_control.py
@@ -6,7 +6,7 @@ Created on Tue May 22 11:35:35 2018
 @author: nvidia
 """
 
-import rospy
+import rospy, yaml
 
 from Adafruit_PCA9685 import PCA9685
 from Servo import ServoMotor
@@ -16,17 +16,63 @@ from std_msgs.msg import String
 
 global servo, dc_motor, pca
 
-STANDSTILL_PWM = 315
+
+path = "../config/settings.yaml"
+settings = yaml.safe_load(open(path))
+dc_motor_pwm = settings['dc_motor']
+servo_pwm = settings['servo']
 
 pwm_freq = 50
-dc_init_pwm = 315
-servo_init_pwm = 310
 
+# DC-motor
+STANDSTILL_PWM = dc_motor_pwm['pwm_midpoint']
+pwm_min = dc_motor_pwm['pwm_min']
+pwm_max = dc_motor_pwm['pwm_max']
+pwm_midrange_min = dc_motor_pwm['pwm_midrange_min']
+pwm_midrange_max = dc_motor_pwm['pwm_midrange_max']
+dc_init_pwm = STANDSTILL_PWM
+mode = 'neutral'
+
+# Servo
+servo_init_pwm = servo_pwm['pwm_midpoint']
+
+def get_reverse_pwm():
+    return (pwm_min, pwm_midrange_min-1)
+
+def get_neutral_pwm():
+    return (pwm_midrange_min, pwm_midrange_max)
+
+def get_forward_pwm():
+    return (pwm_midrange_max+1, pwm_max)
+
+def in_range(_range,pwm_val):
+    return pwm_val >= _range[0] and pwm_val <= _range[1]
+
+def get_mode(pwm_val):
+    if in_range(get_reverse_pwm(),pwm_val):
+        return 'reverse'
+    elif in_range(get_forward_pwm(),pwm_val):
+        return 'forward'
+    else:
+        return 'neutral'
 
 def dc_callback(data):
-    speed_pwm = data.data + dc_motor.get_current_pwm()
+    inc = data.data
+    speed_pwm = inc + dc_motor.get_current_pwm()
+    if mode is 'neutral' and inc>0:
+        speed_pwm = max(get_forward_pwm()[0],speed_pwm)
+        mode = get_mode(speed_pwm)
+    elif mode is 'neutral' and inc<0:
+        speed_pwm = min(get_reverse_pwm()[1],speed_pwm)
+        mode = get_mode(speed_pwm)
+    elif mode is 'forward' and speed_pwm<get_forward_pwm()[0]:
+        speed_pwm = dc_init_pwm
+        mode = 'neutral'
+    elif mode is 'reverse' and speed_pwm>get_reverse_pwm()[1]:
+        speed_pwm = dc_init_pwm
+        mode = 'neutral'
     dc_motor.set_pwm(speed_pwm)
-    print(speed_pwm)
+    print(mode,speed_pwm)
 
 
 def speed_listener():
