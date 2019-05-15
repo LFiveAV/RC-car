6f2992e7b6e62f3d31473eb357bccd050e12d68d
Karl <27566241+krlhrnbrnt@users.noreply.github.com>
2019-05-15 13:32:25 +0200

    PD-controller

--- drive_rc_car/src/WallFollower.py
+++ drive_rc_car/src/WallFollower.py
@@ -15,32 +15,39 @@ import time
 from geometry_msgs.msg import Pose2D
 from std_msgs.msg import Int8
 
+global last_error
+last_error = 0
 
 class WallFollower(object):
 
     def __init__(self, desired_dist):
         self.pub = rospy.Publisher("steering_pwm", Int8, queue_size=1)
         self.lat_kp = 5
+        self.lat_kd = 5
         self.yaw_kp = 2
         self.lateral_dist_des = desired_dist
         self.heading_des = 0
         self.wall_dist = 0
         self.prev_dist = 1
         self.counter = 0
+        self.threshold = 0.01
         self.wall_dist_listener()
 
-    def p_controller(self, lat_dev):
-        lat_control = self.lat_kp * lat_dev
+    def pd_controller(self,dist):
+        error = self.lateral_dist_des - dist
+        error_diff = last_error-error
+        print("PD error: " + str(error))
+        lat_control = self.lat_kp * error + self.lat_kd*error_diff
+        last_error = error
         return lat_control
 
     def callback(self, dist_msg):
         dist = dist_msg.y
         heading = dist_msg.theta
-        lat_dev = self.lateral_dist_des - dist
         yaw_dev = self.heading_des - heading
         print(lat_dev)
         # print(yaw_dev)
-        steering_control = self.p_controller(lat_dev)
+        steering_control = self.pd_controller(dist)
         self.pub.publish(steering_control)
 
     def wall_dist_listener(self):
