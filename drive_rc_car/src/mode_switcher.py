18649dc4285ee5a0ab5724403f7c30989fb19aa3
LFiveAV <49906145+LFiveAV@users.noreply.github.com>
2019-06-26 13:23:31 +0200

    Working version

--- drive_rc_car/src/mode_switcher.py
+++ drive_rc_car/src/mode_switcher.py
@@ -15,16 +15,16 @@ class ModeSwitcher(object):
         self.mode = 'remote' # self.mode must be 'remote' or 'self_drive'
         self.speed_inc = 0
         self.steering_inc = 0
+        self.speed_inc_self_drive = 0
+        self.steering_inc_self_drive = 0
         self.rate = rospy.Rate(10)
 
     def start(self):
         while not rospy.is_shutdown():
             self.mode_listener()
-            if self.mode == 'remote':
-                self.remote_listener()
-            else:
-                self.self_drive_listener()
-            
+            self.remote_listener()
+            self.self_drive_listener()
+            self.pub_control()
             self.rate.sleep()
 
     def mode_listener(self):
@@ -39,30 +39,46 @@ class ModeSwitcher(object):
         rospy.Subscriber('speed_pwm',Int8,self.speed_callback)
         rospy.Subscriber('steering_pwm',Int8,self.steering_callback)
 
-        self.pub_control()
-
+       
     def self_drive_listener(self):
-        rospy.Subscriber('speed_pwm_sd',Int8,self.speed_callback)
-        rospy.Subscriber('steering_pwm_sd',Int8,self.steering_callback)
+        rospy.Subscriber('speed_pwm_sd',Int8,self.speed_callback_sd)
+        rospy.Subscriber('steering_pwm_sd',Int8,self.steering_callback_sd)
 
-        self.pub_control()
 
     def speed_callback(self,data):
         self.speed_inc = data.data
 
     def steering_callback(self,data):
-        self.steering_inc = data.data
+        self.steering_inc  = data.data
+
+    def speed_callback_sd(self,data):
+        self.speed_inc_self_drive = data.data
+
+
+    def steering_callback_sd(self,data):
+        self.steering_inc_self_drive = data.data
 
     def pub_control(self):
         msg = TwoFloat32()
-        msg.data1 = self.speed_inc
-        msg.data2 = self.steering_inc
+        print (self.mode)
+        print ('[remote]', self.speed_inc, self.steering_inc)
+        print ('[self_drive]', self.speed_inc_self_drive, self.steering_inc_self_drive)
+        if self.mode == 'remote':
+            
+            msg.data1 = self.speed_inc
+            msg.data2 = self.steering_inc
+        elif self.mode == 'self_drive':
+            
+            msg.data1 = self.speed_inc_self_drive
+            msg.data2 = self.steering_inc_self_drive
         self.pub.publish(msg)
         self.reset_inc()
 
     def reset_inc(self):
         self.speed_inc = 0
         self.steering_inc = 0
+        self.speed_inc_self_drive = 0
+        self.steering_inc_self_drive = 0
 
 
 if __name__ == '__main__':
